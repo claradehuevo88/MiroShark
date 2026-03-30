3f93555668409bdd6b17dfe73f7ba4df1ac8dfd0
claradehuevo88 <claradehuevo88@gmail.com>
2026-03-30 23:16:36 +0200

    Add allowedHosts to Vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
     open: true,
+    allowedHosts: ['miroshark.onrender.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
